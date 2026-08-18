46c0de6cd885aeb3300161fb8553eeb1f8dbbc9a
Michael Kuehl <hello@micku.me>
2026-08-18 07:10:31 +0200

    added a first simple test

---
+++ tests/boot_test.py
@@ -0,0 +1,33 @@
+import logging
+import sys
+
+import pytest
+
+from jumpstarter_testing.pytest import JumpstarterTest
+
+
+log = logging.getLogger(__name__)
+
+
+class TestBootProcess(JumpstarterTest):
+    selector = "type=qemu"
+
+    def test_boot(self, client):
+        """Test the boot process of the device."""
+        log.info("Testing boot process")
+        client.power.cycle()
+        
+        with client.console.pexpect() as console:
+            # uncomment this if you want to see the console in action while testing
+            console.logfile_read = sys.stdout.buffer
+            console.expect_exact("login:", timeout=120)
+            console.sendline("root")
+            console.expect_exact("Password:", timeout=10)
+            console.sendline("password")
+            console.expect_exact("]#", timeout=10)
+
+    def test_uname(self, client):
+        with client.console.pexpect() as console:
+            console.sendline("uname -a")
+            console.expect_exact("]#", timeout=10)
+            print(console.before.decode())
